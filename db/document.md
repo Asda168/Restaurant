59a8240c2d654cc569f45e984de118b4ccd6ec03
Asda168 <oukasda@gmail.com>
2025-08-03 14:58:10 +0700

    Vue version 3 using json server

---
+++ db/document.md
@@ -0,0 +1,11 @@
+# Websit 
+    https://www.npmjs.com/package/json-server
+# Install Json Global in Terminal:
+    npm install -g json-server
+# Create file json db.json:
+    Create a db.json file to view the JSON data after inserting records.
+# Run the server: 
+    json-server --watch db.json
+
+# Server: 
+    http://localhost:3000/users
---
+++ resto-project/src/components/SignUp.vue
@@ -0,0 +1,53 @@
+<template>
+  <img class="logo" src="../assets/logo.png">
+  <h3>Sign Up</h3>
+  <div class="register">
+    <input type="name" v-model="name" placeholder="Enter your name" />
+    <input type="email" v-model="email" placeholder="E-mail@gmail.com">
+    <input type="password" v-model="password" placeholder="Enter your password" />
+    <button v-on:click="SignUp" class="signup">SignUp</button>
+  </div>
+</template>
+
+<script>
+import axios  from "axios";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      password: "",
+      email: "",
+    }
+  },
+  methods: {
+    SignUp() {
+      console.warn("SignUp", this.name, this.email, this.password);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  color: #fff;
+  background-color: skyblue;
+  cursor: pointer;
+}
+</style>
